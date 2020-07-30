d50daa974207d38fcf6775b9798ba18840795232
Eyassu Getachew <eyassug@gmail.com>
2020-07-30 14:25:31 +0300

    Updated view controls

--- MobileNPC/Views/AboutPage.xaml.cs
+++ MobileNPC/Views/AboutPage.xaml.cs
@@ -2,17 +2,31 @@
 using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
 
 namespace MobileNPC.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class AboutPage : ContentPage
+    public partial class AboutPage : ZXingScannerPage
     {
         public AboutPage()
         {
             InitializeComponent();
         }
+        public void ZXingScannerPage_OnScanResult(ZXing.Result result)
+        {
+            IsScanning = !IsScanning;
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                //await Navigation.PushModalAsync(new AboutPage());
+                await DisplayAlert($"Scanned Result\n", $"Format:{result.BarcodeFormat}\nMetadata{result.ResultMetadata}\n{result.Text}", "OK");
+            });
+            IsScanning = !IsScanning;
+        }
+
     }
+
 }
