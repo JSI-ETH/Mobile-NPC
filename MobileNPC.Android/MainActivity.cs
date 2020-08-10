1a005a271c73f200e7ab6347a74169b16fc34556
Eyassu Getachew <eyassug@gmail.com>
2020-08-10 12:11:39 +0300

    added permissions for Camera and Flashlight, added permission for ZXing.Net

--- MobileNPC.Android/MainActivity.cs
+++ MobileNPC.Android/MainActivity.cs
@@ -27,7 +27,7 @@ namespace MobileNPC.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
