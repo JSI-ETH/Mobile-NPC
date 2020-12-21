30d636ff0034114153d6502a12c5b58043997738
Eyassu Getachew <eyassug@gmail.com>
2020-12-21 13:43:02 +0300

    Reverted AppConstants default values

--- MobileNPC/Core/AppConstants.cs
+++ MobileNPC/Core/AppConstants.cs
@@ -2,11 +2,11 @@
 {
     public class AppConstants
     {
-        public const string AkeneoUrl = "https://npc.sandboxaddis.com";
-        public const string Username = "admin";
-        public const string Password = "Admin123";
-        public const string ClientId = "2_lgmnk110ltcss408g0cw8kwg80ocw0wo48cgsk40o048000oo";
-        public const string ClientSecret = "4xjp4l48sm0wo4csck4k40g8g8cgg488w4o4owsc8cgoogog4s";
-        public const string AkeneoConfigUrl = "https://jsonblob.com/api/jsonBlob/64b8cc98-eec2-11ea-b1f3-9f94751b92dd";
+        public const string AkeneoUrl = "AKENEO_URL";
+        public const string Username = "AKENEO_USERNAME";
+        public const string Password = "AKENEO_PASSWORD";
+        public const string ClientId = "AKENEO_CLIENT_ID";
+        public const string ClientSecret = "AKENEO_CLIENT_SECRET";
+        public const string AkeneoConfigUrl = "AKENEO_CONFIG_URL";
     }
 }
