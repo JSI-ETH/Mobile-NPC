41b8ecca368c1c7bb98c12ded7937582ab7d42ac
Eyassu Getachew <eyassug@gmail.com>
2021-04-16 19:11:47 +0300

    added fix for barcode scanning issue

--- MobileNPC/Views/AboutPage.xaml.cs
+++ MobileNPC/Views/AboutPage.xaml.cs
@@ -17,29 +17,17 @@ namespace MobileNPC.Views
         {
             InitializeComponent();
         }
-        public void ZXingScannerPage_OnScanResult(ZXing.Result result)
+        public async void ZXingScannerPage_OnScanResult(ZXing.Result result)
         {
-            IsScanning = !IsScanning;
-
-            Device.BeginInvokeOnMainThread(async () =>
-            {
-                var gtin = result.Text;
-                var selectedItem = await _dataStore.GetItemAsync(gtin);
-                if(selectedItem != null)
-                {
-                    //MessagingCenter.Send(this, "ItemScanned", selectedItem);
-                    await Navigation.PushAsync(new ItemDetailPage(new ViewModels.ItemDetailViewModel(selectedItem)));
-                    //await Navigation.
-                    //await Navigation.PushAsync(new ItemsPage(new ViewModels.ItemDetailViewModel(selectedItem)));
-                }
-                    
-                else
-                    await DisplayAlert("Sorry!",$"The item with the specified GTIN `{result.Text}` could not be found. Please try again!", "OK");
-               
-            });
-            IsScanning = !IsScanning;
+            IsScanning = false;
+            var gtin = result.Text;
+            var selectedItem = await _dataStore.GetItemAsync(gtin);
+            if (selectedItem != null)
+                await Navigation.PushModalAsync(new NavigationPage(new ItemDetailPage(new ViewModels.ItemDetailViewModel(selectedItem))));
+            else
+                await DisplayAlert("Sorry!", $"The item with the specified GTIN `{result.Text}` could not be found. Please try again!", "OK");
+            IsScanning = true;
         }
-
         
         protected override bool OnBackButtonPressed()
         {
