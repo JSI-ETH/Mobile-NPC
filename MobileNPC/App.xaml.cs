5f79a058bdb6c261d28c3d6125a4992ab484024b
Eyassu Getachew <eyassug@gmail.com>
2021-04-29 19:40:02 +0300

    #7 removed AppCenter Distribute to resolve PlayStore flag

--- MobileNPC/App.xaml.cs
+++ MobileNPC/App.xaml.cs
@@ -66,7 +66,7 @@ namespace MobileNPC
         {
             AppCenter.Start("android=2978adb1-3a4b-4c3f-89fc-22cdd61d0c9c;" +
                   "ios=4675c0bf-7208-4873-969a-235251ceb5b0;",
-                  typeof(Analytics), typeof(Crashes), typeof(Distribute));
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
